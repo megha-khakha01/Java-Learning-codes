f623cfda4ac01fa795066811a122eae3e1ac31e1
Megha Khakha <meghakhakha148@gmail.com>
2025-05-22 19:45:45 +0530

    program1

--- Section_5/Program1.java
+++ Section_5/Program1.java
@@ -3,6 +3,6 @@ package Section_5;
 public class Program1 {
     public static void main(String[] args) {
         
-        System.out.println("megha");
+        System.out.println("hi");
     }
 }
--- Section_6/Program1.java
+++
@@ -1,7 +0,0 @@
-package Section_6;
-
-public class Program1 {
-    public static void main(String[] args) {
-        System.out.println("hello");
-    }
-}
